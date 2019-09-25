1924edbc5795f873a20288763198700336ca50bf
fokamdk99 <stasskrzypek@gmail.com>
2019-09-26 00:14:47 +0200

    heroku changes

--- LabElka3/settings.py
+++ LabElka3/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'labelka/templates/static'),
+    
     os.path.join(BASE_DIR, 'login/templates/static'),
     os.path.join(BASE_DIR, 'home/templates/static'),
     os.path.join(BASE_DIR, 'users/templates/static'),
